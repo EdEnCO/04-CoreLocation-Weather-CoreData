1897552de8465a1645fb8b0003fe3302d0fffe87
GCotumaccio <cotumaccio@gmail.com>
2016-06-23 12:55:22 +0200

    Initial Commit

---
+++ 04-CoreLocation-Weather-CoreDataTests/_4_CoreLocation_Weather_CoreDataTests.swift
@@ -0,0 +1,36 @@
+//
+//  _4_CoreLocation_Weather_CoreDataTests.swift
+//  04-CoreLocation-Weather-CoreDataTests
+//
+//  Created by Gianfranco Cotumaccio on 23/06/16.
+//  Copyright © 2016 Propaganda Studio. All rights reserved.
+//
+
+import XCTest
+@testable import _4_CoreLocation_Weather_CoreData
+
+class _4_CoreLocation_Weather_CoreDataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
